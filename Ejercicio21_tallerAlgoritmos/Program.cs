b9703e05ff9cf14b6ef6b9b83894daae27095f99
davidalzate31 <davidalzate2609@gmail.com>
2024-03-20 21:07:45 -0500

    commits1

---
+++ Ejercicio21_tallerAlgoritmos/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+       /* Se quiere crear un algoritmo en C# que calcule el IMC de N pacientes y luego lo clasifique dentro de una categoría:
+        -Si el IMC del paciente es menor a 18,5, entonces tiene peso insuficiente.
+        -Si el IMC está entre 18,5 y 24,9 se encuentre en la categoría de Peso Saludable
+        -Si el IMC está entre 25,0 y 29,9 entonces tiene sobrepeso
+        - Si el IMC es mayor de 29.9 entonces tiene obesidad.
+        Mostrar en pantalla la categoría de cada uno de los pacientes.La aplicación debe estar en capacidad de pedirle al usuario si quiere volver a calcular */
+
+        //Declaracion de variables.
+
+
+
+
+
+
+
+
+    }
+}
